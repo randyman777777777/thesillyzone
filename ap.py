5a2a190074a7bc4f157dfa53671ece094cb7463c
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-03-09 19:03:40 -0800

    Active

---
+++ ap.py
@@ -0,0 +1,37 @@
+import time
+import threading
+import requests
+
+def connect_to_website(url):
+  while True:
+    r0 = time.time()
+    r1 = requests.get(url)
+    j34 = time.time()
+    r2 = requests.get(url)
+    b6 = time.time()
+
+    e1 = j34 - r0
+    e2 = b6 - j34
+    with open("res.txt", "a") as f:
+      f.write(f'The website took {e1:.2f} seconds to load.')
+      f.write(f"Connected to {url} with status code: {r1.status_code}")
+      f.write(f'The website took {e2:.2f} seconds to load.')
+      f.write(f"Connected to {url} with status code: {r2.status_code}")
+      f.close
+
+def main(num_threads, url):
+    threads = []
+    for i in range(num_threads):
+        t = threading.Thread(target=connect_to_website, args=(url,))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+
+if __name__ == '__main__':
+    print("woooowooooowwowowooooww")
+    b5 = "https://www.google.com/"
+    z1 = 4444
+    z2 = int(z1)
+    url = b5
+    main(z2, url)
